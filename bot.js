7b9a8d9f5ed641b5857c649e4dc0473086f2692d
FrigidApple <117215428+FrigidApple@users.noreply.github.com>
2023-03-24 15:17:33 -0400

    Pushing command handling and basic command test

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
+const path = require('node:path');
 const { token } = require('./config.json');
-const { Client, GatewayIntentBits } = require('discord.js');
-const path = require('path');
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
 const fs = require('node:fs');
 const CronJob = require('./node_modules/cron/lib/cron.js').CronJob;
 
@@ -15,6 +15,41 @@ const client = new Client({
         
     ]
 });
+
+//
+
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	// Set a new item in the Collection with the key as the command name and the value as the exported module
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	} else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
+}
+
+client.on(Event.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
+
+
 
 const eventsPath = path.join(__dirname, 'events');
 const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
